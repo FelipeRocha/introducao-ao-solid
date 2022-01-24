a1423ca19f3f0e11820bada8fc56ffdf0ace231d
Felipe Rocha <felipearaujo289@gmail.com>
2022-01-23 21:35:40 -0300

    Desafio completo

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isUserAdmin = this.usersRepository.findById(user_id)?.admin;
+    if (isUserAdmin === false) {
+      throw new Error("Usuário não é administrador!");
+    }
+
+    if (!isUserAdmin) {
+      throw new Error("Usuário não encontrado!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
